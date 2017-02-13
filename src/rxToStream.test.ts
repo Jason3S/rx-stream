7cc56771ec4811008e6d1e4f97a8898190a8fb47
Jason Dent <jason@streetsidesoftware.nl>
2017-02-13 23:16:53 +0100

    Fix issue with running out of stack space.

--- src/rxToStream.test.ts
+++ src/rxToStream.test.ts
@@ -20,4 +20,22 @@ describe('Validate to Stream', () => {
 
         return p;
     });
+
+    it('rxToStream large range', () => {
+        // This tests that we can send many small values to the stream on after another.
+        // This is to make sure we do not run out of stack space.
+        const max = 5000;
+        const src = Rx.Observable.range(1, max);
+        const stream = rxToStream(src.map(a => a.toString()));
+
+        const p = streamToStringRx(stream)
+            .map(a => Number.parseInt(a))
+            .reduce((a, b) => a + b)
+            .toPromise()
+            .then(result => {
+                expect(result).to.equal((max * (max + 1)) / 2);
+            });
+
+        return p;
+    });
 });
--- src/rxToStream.ts
+++ src/rxToStream.ts
@@ -1,12 +1,16 @@
 import * as Rx from 'rxjs/Rx';
 import * as stream from 'stream';
 
+export type Streamable = string | Buffer;
+
 /**
  * Transform the output of an Observable into a node readable stream.
  */
-export function rxToStream<T>(src: Rx.Observable<T>, options: stream.ReadableOptions = { encoding: 'utf8' }): stream.Readable {
+export function rxToStream<T extends Streamable>(src: Rx.Observable<T>, options: stream.ReadableOptions = { encoding: 'utf8' }): stream.Readable {
 
     const trigger = new Rx.Subject<void>();
+    let depth = 0;
+    const maxDepth = 100;
 
     const readable = new stream.Readable({
         ...options,
@@ -15,15 +19,39 @@ export function rxToStream<T>(src: Rx.Observable<T>, options: stream.ReadableOpt
         },
     });
 
+    function close() {
+        Rx.Observable.interval(1).take(1).subscribe(
+            () => readable.push(null)
+        );
+    }
+
+    function next() {
+        if (depth < maxDepth) {
+            // Use the fastest method
+            trigger.next();
+        } else {
+            // Slower to avoid running out of stack space.
+            Rx.Observable.interval().take(1).subscribe(
+                () => trigger.next()
+            );
+        }
+    }
+
+    function push(data: T) {
+        depth += 1;
+        readable.push(data) ? next() : null;
+        depth -= 1;
+    }
+
     trigger
         // Use zip to buffer the Observable so we only send when the stream is ready.
         .zip(src, (_, src) => src)
         .subscribe(
             // send the data and signal we can use more data.
-            data => readable.push(data) ? trigger.next() : null,
+            push,
             // Close on error or complete.
-            () => readable.push(null),
-            () => readable.push(null)
+            close,
+            close
         );
 
     return readable;
