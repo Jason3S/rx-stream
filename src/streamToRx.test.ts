71044c40e1f7ff2f42e9e2c90a2c1a8ccd49911a
Jason Dent <jason@streetsidesoftware.nl>
2019-05-15 00:07:12 +0200

    Fix #7

--- src/streamToRx.test.ts
+++ src/streamToRx.test.ts
@@ -7,7 +7,7 @@ describe('Validate Rx From Stream', () => {
     it('tests stream to Rx', () => {
         const data: string = 'This is a bit of text to have some fun with';
         const bufferStream = new stream.PassThrough();
-        bufferStream.end(data);
+        bufferStream.end(data, 'utf8');
         return streamToStringRx(bufferStream)
             .pipe(reduce((a, b) => a + b))
             .toPromise()
@@ -16,9 +16,27 @@ describe('Validate Rx From Stream', () => {
             });
     });
 
-    it('should not be a subject', () => {
+    it('tests closing a stream', async () => {
+        const data: string = 'This is a bit of text to have some fun with';
         const bufferStream = new stream.PassThrough();
         const observable = streamToStringRx(bufferStream);
+        bufferStream.write(data, 'utf8');
+        const result = observable
+            .pipe(reduce((a, b) => a + b))
+            .toPromise();
+        bufferStream.destroy();
+        expect(await result).to.equal(data);
+    });
+
+    it('should not be a subject', async () => {
+        const data = 'Some sample text';
+        const bufferStream = new stream.PassThrough();
+        const observable = streamToStringRx(bufferStream, 'utf8');
         expect((observable as any).next).not.to.be.a('function');
+        bufferStream.end(data, 'utf8');
+        const result = await observable
+            .pipe(reduce((a, b) => a + b))
+            .toPromise();
+        expect(result).to.equal(data);
     });
 });
--- src/streamToRx.ts
+++ src/streamToRx.ts
@@ -8,18 +8,22 @@ export function streamToRx<T = Buffer>(stream: NodeJS.ReadableStream): Observabl
         const dataHandler = (data: T) => subscriber.next(data);
 
         stream.addListener('end', endHandler);
+        stream.addListener('close', endHandler);
         stream.addListener('error', errorHandler);
         stream.addListener('data', dataHandler);
 
         return () => {
             stream.removeListener('end', endHandler);
+            stream.removeListener('close', endHandler);
             stream.removeListener('error', errorHandler);
             stream.removeListener('data', dataHandler);
         };
     });
 }
 
-export function streamToStringRx(stream: NodeJS.ReadableStream, encoding: string = 'UTF-8'): Observable<string> {
+export function streamToStringRx(stream: NodeJS.ReadableStream, encoding?: string): Observable<string>;
+export function streamToStringRx(stream: NodeJS.ReadableStream, encoding: BufferEncoding): Observable<string>;
+export function streamToStringRx(stream: NodeJS.ReadableStream, encoding: string = 'utf8'): Observable<string> {
     return streamToRx(stream)
         .pipe(map(buffer => buffer.toString(encoding)));
 }
